0be22514de5070f5eee0c1f2adbd5a21f74835b9
Suriyaa MM <suriyaamm.2705@gmail.com>
2025-08-29 17:56:45 +0530

    round 1046 div 2

---
+++ Codeforces/A_In_the_Dream.cpp
@@ -0,0 +1,125 @@
+#include <algorithm>
+#include <cmath>
+#include <cstdint>
+#include <iostream>
+#include <limits>
+#include <map>
+#include <queue>
+#include <set>
+#include <string>
+#include <unordered_map>
+#include <unordered_set>
+#include <utility>
+#include <vector>
+
+// reads the vector
+#define read_vec(begin, end, vec)         \
+    for (int64_t i = begin; i < end; ++i) \
+        std::cin >> vec[i];
+
+// reads the vector pair
+#define read_vecp(begin, end, vec)        \
+    for (int64_t i = begin; i < end; ++i) \
+        std::cin >> vec[i].first >> vec[i].second;
+
+// prints the vector
+#define print_vec(begin, end, vec)        \
+    for (int64_t i = begin; i < end; ++i) \
+        std::cout << vec[i] << " ";
+
+// infinity
+constexpr const int64_t inf = std::numeric_limits<int64_t>::max();
+// negative infinity
+constexpr const int64_t ninf = std::numeric_limits<int64_t>::min();
+
+// pair
+using pair_t = std::pair<int64_t, int64_t>;
+// generic pair
+template <typename T1, typename T2>
+using gpair_t = std::pair<T1, T2>;
+// unweighted graph
+using graph_t = std::vector<std::vector<int64_t>>;
+// weighted graph
+using wgraph_t = std::vector<std::vector<pair_t>>;
+
+// disjoint set union by  & path compression
+struct dsur_t {
+
+    std::vector<int64_t> parent, rank;
+
+    dsur_t(int64_t n) {
+        // O(n)
+        parent.resize(n + 1);
+        // O(n)
+        rank.resize(n + 1, 0);
+
+        for (int64_t i = 1; i <= n; ++i) {
+            parent[i] = i;
+        }
+    }
+    // O(1)
+    int64_t find_parent(int64_t x) {
+        if (parent[x] != x)
+            parent[x] = find_parent(parent[x]);
+        return parent[x];
+    }
+
+    void unite(int64_t u, int64_t v) {
+        u = find_parent(u);
+        v = find_parent(v);
+
+        if (v == u)
+            return;
+
+        if (rank[u] < rank[v])
+            std::swap(u, v);
+
+        parent[v] = u;
+        if (rank[u] == rank[v])
+            ++rank[u];
+    }
+};
+
+void solve() {
+    int64_t a = 0L, b = 0L, c = 0L, d = 0L;
+    std::cin >> a >> b >> c >> d;
+
+    int64_t bigger = std::max(a, b);
+    int64_t smaller = std::min(a, b);
+
+    if (bigger > 2 * smaller + 2) {
+        std::cout << "NO" << std::endl;
+        return;
+    }
+
+    c -= a;
+    d -= b;
+
+    bigger = std::max(c, d);
+    smaller = std::min(c, d);
+
+    if (bigger > 2 * smaller + 2) {
+        std::cout << "NO" << std::endl;
+        return;
+    }
+
+    std::cout << "YES" << std::endl;
+}
+
+int main(int, char**) {
+    std::ios::sync_with_stdio(false);
+    std::cin.tie(nullptr);
+    std::cout.tie(nullptr);
+#define MT
+#ifdef MT
+    int64_t tt = 0L;
+    std::cin >> tt;
+    while (tt--) {
+        solve();
+    }
+#else
+    solve();
+#endif
+
+    return 0;
+}
---
+++ Codeforces/B_Like_the_Bitset.cpp
@@ -0,0 +1,167 @@
+#include <algorithm>
+#include <cmath>
+#include <cstdint>
+#include <iostream>
+#include <limits>
+#include <map>
+#include <queue>
+#include <set>
+#include <string>
+#include <unordered_map>
+#include <unordered_set>
+#include <utility>
+#include <vector>
+
+// reads the vector
+#define read_vec(begin, end, vec)         \
+    for (int64_t i = begin; i < end; ++i) \
+        std::cin >> vec[i];
+
+// reads the vector pair
+#define read_vecp(begin, end, vec)        \
+    for (int64_t i = begin; i < end; ++i) \
+        std::cin >> vec[i].first >> vec[i].second;
+
+// prints the vector
+#define print_vec(begin, end, vec)        \
+    for (int64_t i = begin; i < end; ++i) \
+        std::cout << vec[i] << " ";
+
+// infinity
+constexpr const int64_t inf = std::numeric_limits<int64_t>::max();
+// negative infinity
+constexpr const int64_t ninf = std::numeric_limits<int64_t>::min();
+
+// pair
+using pair_t = std::pair<int64_t, int64_t>;
+// generic pair
+template <typename T1, typename T2>
+using gpair_t = std::pair<T1, T2>;
+// unweighted graph
+using graph_t = std::vector<std::vector<int64_t>>;
+// weighted graph
+using wgraph_t = std::vector<std::vector<pair_t>>;
+
+// disjoint set union by  & path compression
+struct dsur_t {
+
+    std::vector<int64_t> parent, rank;
+
+    dsur_t(int64_t n) {
+        // O(n)
+        parent.resize(n + 1);
+        // O(n)
+        rank.resize(n + 1, 0);
+
+        for (int64_t i = 1; i <= n; ++i) {
+            parent[i] = i;
+        }
+    }
+    // O(1)
+    int64_t find_parent(int64_t x) {
+        if (parent[x] != x)
+            parent[x] = find_parent(parent[x]);
+        return parent[x];
+    }
+
+    void unite(int64_t u, int64_t v) {
+        u = find_parent(u);
+        v = find_parent(v);
+
+        if (v == u)
+            return;
+
+        if (rank[u] < rank[v])
+            std::swap(u, v);
+
+        parent[v] = u;
+        if (rank[u] == rank[v])
+            ++rank[u];
+    }
+};
+
+void solve() {
+    int64_t n = 0L, k = 0L;
+    std::cin >> n >> k;
+
+    std::string s = "";
+    std::cin >> s;
+
+    // // check whether there are k consecutive 1's using sliding window
+    // int64_t l = 0L, r = 0L, co = 0L, o = 0L;
+    // while (l < r && r < n) {
+    //     if (s[r] == '0') {
+    //         ++r;
+    //         l = r;
+    //         co = std::max(co, (l - r + 1));
+    //     } else {
+    //         ++r;
+    //     }
+    // }
+
+    // for (int64_t i = 0; i < n; ++i) {
+    //     if (s[i] == '1') {
+    //         ++o;
+    //     }
+    // }
+
+    // if (co >= k || o > k) {
+    //     std::cout << "NO" << std::endl;
+    //     return;
+    // }
+
+    int64_t co = 0, maxco = 0;
+    for (int64_t i = 0; i < n; i++) {
+        if (s[i] == '1') {
+            co++;
+            maxco = std::max(maxco, co);
+        } else {
+            co = 0;
+        }
+    }
+
+    if (maxco >= k) {
+        std::cout << "NO" << std::endl;
+        return;
+    }
+
+    std::cout << "YES" << std::endl;
+    std::vector<int64_t> answer(n, 0);
+    int64_t counter = 1;
+    for (int64_t i = 0; i < n; ++i) {
+        if (s[i] == '1') {
+            answer[i] = counter;
+            ++counter;
+        }
+    }
+    for (int64_t i = 0; i < n; ++i) {
+        if (s[i] == '0') {
+            answer[i] = counter;
+            ++counter;
+        }
+    }
+
+    for (const auto& a : answer) {
+        std::cout << a << " ";
+    }
+    std::cout << std::endl;
+}
+
+int main(int, char**) {
+    std::ios::sync_with_stdio(false);
+    std::cin.tie(nullptr);
+    std::cout.tie(nullptr);
+
+#define MT
+#ifdef MT
+    int64_t tt = 0L;
+    std::cin >> tt;
+    while (tt--) {
+        solve();
+    }
+#else
+    solve();
+#endif
+
+    return 0;
+}
