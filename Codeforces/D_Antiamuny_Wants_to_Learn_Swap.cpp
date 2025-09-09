5835dae1e67552e96af6803d2af1ed7f46eec64b
Suriyaa MM <suriyaamm.2705@gmail.com>
2025-09-09 19:52:56 +0530

    september 9

---
+++ Codeforces/D_Antiamuny_Wants_to_Learn_Swap.cpp
@@ -0,0 +1,134 @@
+#include <algorithm>
+#include <cmath>
+#include <cstdint>
+#include <iostream>
+#include <limits>
+#include <map>
+#include <queue>
+#include <set>
+#include <string>
+#include <unordered_map>
+#include <unordered_set>
+#include <utility>
+#include <vector>
+
+// reads the vector
+#define read_vec(begin, end, vec)         \
+    for (int64_t i = begin; i < end; ++i) \
+        std::cin >> vec[i];
+
+// reads the vector pair
+#define read_vecp(begin, end, vec)        \
+    for (int64_t i = begin; i < end; ++i) \
+        std::cin >> vec[i].first >> vec[i].second;
+
+// prints the vector
+#define print_vec(begin, end, vec)        \
+    for (int64_t i = begin; i < end; ++i) \
+        std::cout << vec[i] << " ";
+
+// infinity
+constexpr const int64_t inf = std::numeric_limits<int64_t>::max();
+// negative infinity
+constexpr const int64_t ninf = std::numeric_limits<int64_t>::min();
+
+// pair
+using pair_t = std::pair<int64_t, int64_t>;
+// generic pair
+template <typename T1, typename T2>
+using gpair_t = std::pair<T1, T2>;
+// unweighted graph
+using graph_t = std::vector<std::vector<int64_t>>;
+// weighted graph
+using wgraph_t = std::vector<std::vector<pair_t>>;
+
+// disjoint set union by  & path compression
+struct dsur_t {
+
+    std::vector<int64_t> parent, rank;
+
+    dsur_t(int64_t n) {
+        // O(n)
+        parent.resize(n + 1);
+        // O(n)
+        rank.resize(n + 1, 0);
+
+        for (int64_t i = 1; i <= n; ++i) {
+            parent[i] = i;
+        }
+    }
+    // O(1)
+    int64_t find_parent(int64_t x) {
+        if (parent[x] != x)
+            parent[x] = find_parent(parent[x]);
+        return parent[x];
+    }
+
+    void unite(int64_t u, int64_t v) {
+        u = find_parent(u);
+        v = find_parent(v);
+
+        if (v == u)
+            return;
+
+        if (rank[u] < rank[v])
+            std::swap(u, v);
+
+        parent[v] = u;
+        if (rank[u] == rank[v])
+            ++rank[u];
+    }
+};
+
+void solve() {
+    int64_t n = 0LL, q = 0LL;
+    std::cin >> n >> q;
+
+    std::vector<int64_t> a(n, 0LL);
+    read_vec(0, n, a);
+
+    std::vector<int64_t> bad(n, 0LL);
+    for (int64_t i = 0; i <= n - 3; ++i) {
+        if (a[i] > a[i + 1] && a[i + 1] > a[i + 2]) {
+            bad[i] = 1;
+        }
+    }
+
+    std::vector<int64_t> prefix(n + 1, 0);
+    for (int64_t i = 0; i < n; ++i) {
+        prefix[i + 1] = prefix[i] + bad[i];
+    }
+
+    for (int64_t i = 0; i < q; ++i) {
+        int64_t l = 0LL, r = 0LL;
+        std::cin >> l >> r;
+
+        if (r - l + 1 < 3) {
+            std::cout << "YES\n";
+        } else {
+            if (prefix[(r - 3) + 1] - prefix[l - 1] > 0) {
+                std::cout << "NO\n";
+            } else {
+                std::cout << "YES\n";
+            }
+        }
+    }
+}
+
+int main(int, char**) {
+    std::ios::sync_with_stdio(false);
+    std::cin.tie(nullptr);
+    std::cout.tie(nullptr);
+#define MT
+#ifdef MT
+    int64_t tt = 0L;
+    std::cin >> tt;
+    while (tt--) {
+        solve();
+    }
+#else
+    solve();
+#endif
+
+    return 0;
+}
